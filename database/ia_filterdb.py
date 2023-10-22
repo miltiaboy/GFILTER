01c8658341302ab97626ae6589edf0ecefa11f2e
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-22 20:28:57 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -111,7 +111,7 @@ async def get_search_results(query, file_type=None, max_results=(MAX_BTN), offse
     return files, next_offset, total_results
 
 
-async def get_bad_files(query, file_type=None, max_results=100, offset=0, filter=False):
+async def get_bad_files(query, file_type=None, max_results=5, offset=0, filter=False):
     """For given query return (results, next_offset)"""
     query = query.strip()
     #if filter:
