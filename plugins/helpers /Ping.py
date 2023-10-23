010854eaba5795bb667779cd525e3c5cd76bbdcd
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-23 20:31:12 +0530

    Create Ping.py

---
+++ plugins/helpers /Ping.py
@@ -0,0 +1,18 @@
+import asyncio
+import time
+import random
+from pyrogram import Client, filters
+
+CMD = ["/", "."]
+BOT_START_TIME = time.time()
+
+@Client.on_message(filters.command("ping", CMD))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"<b><i>🔥 Lɪᴠᴇ Pɪɴɢ Iꜱ {time_taken_s:.3f}</i></b>")
+    await asyncio.sleep(9)
+    await rm.delete()
+    await message.delete()
