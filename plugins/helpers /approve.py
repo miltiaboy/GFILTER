bedb9a691b00c38d300823d9b4ba790b8f93b55c
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-23 23:25:10 +0530

    Update approve.py

--- plugins/helpers /approve.py
+++ plugins/helpers /approve.py
@@ -12,21 +12,21 @@ async def autoapprove(client, message: ChatJoinRequest):
     chat=message.chat 
     user=message.from_user 
     print(f"{user.first_name} Joined (Approved)") 
-    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)  
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)   
+    T = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+    Time = T.hour        
+    if Time < 12:
+        afsu="Gᴏᴏᴅ Mᴏʀɴɪɴɢ" 
+    elif Time < 15:
+        afsu="Gᴏᴏᴅ AғᴛᴇʀNᴏᴏɴ" 
+    elif Time < 20:
+        afsu="Gᴏᴏᴅ Eᴠᴇɴɪɴɢ"
+    else:
+        afsu="Gᴏᴏᴅ Nɪɢʜᴛ"    
     buttons = [[
                 InlineKeyboardButton('• Jᴏɪɴ Gʀᴏᴜᴘ¹ •', url='https://t.me/+pyrfAbBUsMExOTZl'),       
                 InlineKeyboardButton('• Jᴏɪɴ Gʀᴏᴜᴘ² •', url='https://t.me/+_B8Y75f6gGQ5MjU1')
-            ]]    
-    T = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
-        Time = T.hour        
-        if Time < 12:
-            afsu="Gᴏᴏᴅ Mᴏʀɴɪɴɢ" 
-        elif Time < 15:
-            afsu="Gᴏᴏᴅ AғᴛᴇʀNᴏᴏɴ" 
-        elif Time < 20:
-            afsu="Gᴏᴏᴅ Eᴠᴇɴɪɴɢ"
-        else:
-            afsu="Gᴏᴏᴅ Nɪɢʜᴛ"    
+            ]]        
     k = await client.send_photo(
         photo=random.choice(PICS),
         chat_id=message.from_user.id, 
