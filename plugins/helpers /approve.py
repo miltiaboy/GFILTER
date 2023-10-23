28e8e4b3518941e125a735ccab27ba99b8006ed3
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-23 23:08:32 +0530

    Update approve.py

--- plugins/helpers /approve.py
+++ plugins/helpers /approve.py
@@ -1,8 +1,11 @@
 import os
 import asyncio
+import random
+import pytz
+import datetime
 from pyrogram import Client, filters
 from pyrogram.types import Message, User, InlineKeyboardMarkup, InlineKeyboardButton, ChatJoinRequest
-from info import CHAT_ID, TEXT, APPROVED 
+from info import CHAT_ID, TEXT, APPROVED, PICS
 
 @Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
 async def autoapprove(client, message: ChatJoinRequest):
@@ -11,14 +14,26 @@ async def autoapprove(client, message: ChatJoinRequest):
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)  
     buttons = [[
-                InlineKeyboardButton('🔮 Jᴏɪɴ Mᴏᴠɪᴇs Gʀᴏᴜᴘ 🔮', url='https://t.me/KL_GROUP1')
-              ],[       
-                InlineKeyboardButton('💥 Jᴏɪɴ Mᴏᴠɪᴇs Cʜᴀɴɴᴇʟ 💥', url='https://t.me/Team_KL')
-            ]]
-    await client.send_photo(
-        photo="https://telegra.ph/file/39e246de80d814ff8bdfd.jpg",
+                InlineKeyboardButton('• Jᴏɪɴ Gʀᴏᴜᴘ¹ •', url='https://t.me/+pyrfAbBUsMExOTZl'),       
+                InlineKeyboardButton('• Jᴏɪɴ Gʀᴏᴜᴘ² •', url='https://t.me/+_B8Y75f6gGQ5MjU1')
+            ]]    
+    T = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+        Time = T.hour        
+        if Time < 12:
+            afsu="Gᴏᴏᴅ Mᴏʀɴɪɴɢ" 
+        elif Time < 15:
+            afsu="Gᴏᴏᴅ AғᴛᴇʀNᴏᴏɴ" 
+        elif Time < 20:
+            afsu="Gᴏᴏᴅ Eᴠᴇɴɪɴɢ"
+        else:
+            afsu="Gᴏᴏᴅ Nɪɢʜᴛ"    
+    k = await client.send_photo(
+        photo=random.choice(PICS),
         chat_id=message.from_user.id, 
-        caption=TEXT.format(mention=user.mention, title=chat.title),
+        caption=TEXT.format(afsu, mention=user.mention, title=chat.title),
         reply_markup=InlineKeyboardMarkup(buttons)
         )   
+    await asyncio.sleep(7)
+    await k.delete()
+    await message.delete()
     
