8333327c429e931a508e9d1e7aae7ff509cf8d37
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-23 21:45:46 +0530

    Update approve.py

--- plugins/helpers /approve.py
+++ plugins/helpers /approve.py
@@ -10,6 +10,16 @@ async def autoapprove(client, message: ChatJoinRequest):
     chat=message.chat 
     user=message.from_user 
     print(f"{user.first_name} Joined (Approved)") 
-    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)  
+    buttons = [[
+                InlineKeyboardButton('🔮 Jᴏɪɴ Mᴏᴠɪᴇs Gʀᴏᴜᴘ 🔮', url='https://t.me/KL_GROUP1')
+              ],[       
+                InlineKeyboardButton('💥 Jᴏɪɴ Mᴏᴠɪᴇs Cʜᴀɴɴᴇʟ 💥', url='https://t.me/Team_KL')
+            ]]
+    await client.send_photo(
+        photo="https://telegra.ph/file/39e246de80d814ff8bdfd.jpg",
+        chat_id=message.from_user.id, 
+        caption=TEXT.format(mention=user.mention, title=chat.title),
+        reply_markup=InlineKeyboardMarkup(buttons)
+        )   
+    
