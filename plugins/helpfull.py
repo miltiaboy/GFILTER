c8f70d655b05340a9497280313c68c28a02ba78b
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-10-25 00:34:13 +0530

    Update helpfull.py

--- plugins/helpfull.py
+++ plugins/helpfull.py
@@ -2,13 +2,13 @@ import random
 from pyrogram import Client, filters
 from info import ADMINS 
 
-@Client.on_message(filters.new_chat_members)
-async def welcome(bot, message):
-	await message.delete()	
+#@Client.on_message(filters.new_chat_members)
+#async def welcome(bot, message):
+	#await message.delete()	
 	
-@Client.on_message(filters.left_chat_member)
-async def goodbye(bot, message):
-	await message.delete()
+#@Client.on_message(filters.left_chat_member)
+#async def goodbye(bot, message):
+	#await message.delete()
 
 @Client.on_message(filters.regex("http") & filters.regex("www") | filters.regex("https") | filters.regex("t.me") & filters.incoming)
 async def nolink(bot, message):
