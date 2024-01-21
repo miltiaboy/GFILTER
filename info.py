fdde897d2dde3b95cc9c52c0885dd80a21ad5c2f
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2024-01-22 02:06:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', 'None')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7d19ddddd9d5033edad19.jpg https://telegra.ph/file/3bdd79ec55a47999bbb2d.jpg https://telegra.ph/file/449737ddea3ba8ed10319.jpg https://telegra.ph/file/06e1e59eef3d94b01240d.jpg https://telegra.ph/file/2eb512df2a55d502449b1.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/c754e9fdceffbd57ea69e.jpg https://telegra.ph/file/62e07bb72c3a4247e7318.jpg https://telegra.ph/file/a6c7ba79a2046abf693e4.jpg https://telegra.ph/file/06e1e59eef3d94b01240d.jpg https://telegra.ph/file/61dbab0c79fedd5df4bce.jpg')).split()
 APICS = (environ.get('APICS', 'https://telegra.ph/file/3e6e5b25be2bc72c7d3d3.jpg https://telegra.ph/file/204cb2f3e2086cc9e2582.jpg')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/60d2e897bfdf063f81545.jpg")
 SPELL_IMG = environ.get('SPELL_IMG',"https://graph.org/file/cd9d7825d4c11195c19df.jpg")
