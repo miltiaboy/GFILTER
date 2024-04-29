9f5e71f4be1dfd518ed08d8cf84d335ec4edbfa3
HacKer Jr ~ TG <83758692+aflahaflah@users.noreply.github.com>
2024-04-29 19:04:25 +0530

    🎇

--- Script.py
+++ Script.py
@@ -176,9 +176,7 @@ Name - {}"""
 ◉) OTT Or DVD Not Released..!
 ◉) Type Name With Year..!
 ◉) Check Your Correct Spelling..!
-◉) Movie Is Not Available in My Database.. Report To Admin @KLAdmin1Bot</b>```
-Don't Ask Theater Print 🥴..!```
-"""
+◉) Movie Is Not Available in My Database.. Report To Admin @KLAdmin1Bot</b>"""
 
     CUDNT_FND = """<b>➠ No Movie Found For Your Query <u>{}</u>
 
