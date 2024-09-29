07d15fa67c3931a356e7d05cce9f4169a50c071c
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-09-30 02:05:23 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -3,98 +3,105 @@ import datetime
 import time
 from database.users_chats_db import db
 from info import ADMINS
-from utils import broadcast_messages
+from utils import users_broadcast, groups_broadcast, temp, get_readable_time
 import asyncio
-from pyrogram.errors import FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup 
 
-@Client.on_message(filters.command("bcspeed") & filters.user(ADMINS) & filters.reply)
-# pari
-async def speed_verupikkals(bot, message):
-    users = await db.get_all_users()
-    b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your Messages With Speed ⚡️'
-    )
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    success = 0
-    failed = 0
-    async for user in users:
-        try:
-            await b_msg.copy(chat_id=int(user['id']))
-            success += 1
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            await b_msg.copy(chat_id=int(user['id']))
-        except Exception as e:
-            failed += 1
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Completed\nTotal : {total_users}\nSuccess : {success}\nFailed : {failed}\nTime Taken : {time_taken}")
+lock = asyncio.Lock()
 
+@Client.on_callback_query(filters.regex(r'^broadcast_cancel'))
+async def broadcast_cancel(bot, query):
+    _, ident = query.data.split("#")
+    if ident == 'users':
+        await query.message.edit("ᴛʀʏɪɴɢ ᴛᴏ ᴄᴀɴᴄᴇʟ ᴜsᴇʀs ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ...")
+        temp.USERS_CANCEL = True
+    elif ident == 'groups':
+        temp.GROUPS_CANCEL = True
+        await query.message.edit("ᴛʀʏɪɴɢ ᴛᴏ ᴄᴀɴᴄᴇʟ ɢʀᴏᴜᴘs ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ...")
+       
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
-async def verupikkals(bot, message):
+async def broadcast_users(bot, message):
+    if lock.locked():
+        return await message.reply('Currently broadcast processing, Wait for complete.')
+
+    p = await message.reply('<b>Do you want pin this message in users?</b>', reply_markup=ReplyKeyboardMarkup([['Yes', 'No']], one_time_keyboard=True, resize_keyboard=True))
+    msg = await bot.listen(chat_id=message.chat.id, user_id=message.from_user.id)
+    if msg.text == 'Yes':
+        is_pin = True
+    elif msg.text == 'No':
+        is_pin = False
+    else:
+        await p.delete()
+        return await message.reply_text('Wrong Response!')
+    await p.delete()
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
+    b_sts = await message.reply_text(text='<b>ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs ᴛᴏ ᴜsᴇʀs ⌛️</b>')
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
     blocked = 0
     deleted = 0
-    failed =0
-
+    failed = 0
     success = 0
-    async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
-        if pti:
-            success += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
+
+    async with lock:
+        async for user in users:
+            time_taken = get_readable_time(time.time()-start_time)
+            if temp.USERS_CANCEL:
+                temp.USERS_CANCEL = False
+                await b_sts.edit(f"Users broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
+                return
+            sts = await users_broadcast(int(user['id']), b_msg, is_pin)
+            if sts == 'Success':
+                success += 1
+            elif sts == 'Error':
                 failed += 1
-        done += 1
-        await asyncio.sleep(2)
-        if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+            done += 1
+            if not done % 20:
+                btn = [[
+                    InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#users')
+                ]]
+                await b_sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>", reply_markup=InlineKeyboardMarkup(btn))
+        await b_sts.edit(f"Users broadcast completed.\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
 
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
-async def group_broadcast(bot, message):
+async def broadcast_group(bot, message):
+    p = await message.reply('<b>Do you want pin this message in groups?</b>', reply_markup=ReplyKeyboardMarkup([['Yes', 'No']], one_time_keyboard=True, resize_keyboard=True))
+    msg = await bot.listen(chat_id=message.chat.id, user_id=message.from_user.id)
+    if msg.text == 'Yes':
+        is_pin = True
+    elif msg.text == 'No':
+        is_pin = False
+    else:
+        await p.delete()
+        return await message.reply_text('Wrong Response!')
+    await p.delete()
     chats = await db.get_all_chats()
     b_msg = message.reply_to_message
-    sts = await message.reply_text('Broadcasting your messages...')
+    b_sts = await message.reply_text(text='<b>ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs ᴛᴏ ɢʀᴏᴜᴘs ⏳</b>')
+    start_time = time.time()
     total_chats = await db.total_chat_count()
     done = 0
-    failed =0
+    failed = 0
     success = 0
-    start_time = time.time()
-    async for chat in chats:
-        chat_id = int(chat['id'])
-        try:
-            await b_msg.copy(chat_id)
-            success += 1
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await b_msg.copy(chat_id)
-        except PeerIdInvalid:
-            await db.delete_user(int(user_id))
-            logging.info(f"{chat_id} - PeerIdInvalid")
-            failed += 1
-        except Exception as e:
-            failed += 1
-        done += 1
-        if not done % 100:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Chats {total_users}\nCompleted: {done}/{total_chats}\nSuccess: {success}\nFailed: {failed}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.delete()
-    await message.reply_text(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}", quote=True)
-
-
-
+    
+    async with lock:
+        async for chat in chats:
+            time_taken = get_readable_time(time.time()-start_time)
+            if temp.GROUPS_CANCEL:
+                temp.GROUPS_CANCEL = False
+                await b_sts.edit(f"Groups broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
+                return
+            sts = await groups_broadcast(int(chat['id']), b_msg, is_pin)
+            if sts == 'Success':
+                success += 1
+            elif sts == 'Error':
+                failed += 1
+            done += 1
+            if not done % 20:
+                btn = [[
+                    InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#groups')
+                ]]
+                await b_sts.edit(f"Groups groadcast in progress...\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>", reply_markup=InlineKeyboardMarkup(btn))    
+        await b_sts.edit(f"Groups broadcast completed.\nCompleted in {time_taken}\n\nTotal Groups: <code>{total_chats}</code>\nCompleted: <code>{done} / {total_chats}</code>\nSuccess: <code>{success}</code>\nFailed: <code>{failed}</code>")
