ab30e26c601d5e0f465018618e0f12542eb97de3
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-09-30 01:40:13 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,48 +1,69 @@
 from pyrogram import Client, filters
+import datetime
+import time
 from database.users_chats_db import db
 from info import ADMINS
-import asyncio, datetime, time
-        
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast_function(bot, message):
+from utils import broadcast_messages
+import asyncio
+from pyrogram.errors import FloodWait
+
+@Client.on_message(filters.command("bcspeed") & filters.user(ADMINS) & filters.reply)
+# pari
+async def speed_verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    sts = await message.reply_text("Broadcasting your messages...")
+    sts = await message.reply_text(
+        text='Broadcasting your Messages With Speed ⚡️'
+    )
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    success = 0
+    failed = 0
+    async for user in users:
+        try:
+            await b_msg.copy(chat_id=int(user['id']))
+            success += 1
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            await b_msg.copy(chat_id=int(user['id']))
+        except Exception as e:
+            failed += 1
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Completed\nTotal : {total_users}\nSuccess : {success}\nFailed : {failed}\nTime Taken : {time_taken}")
+
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+# https://t.me/GetTGLink/4178
+async def verupikkals(bot, message):
+    users = await db.get_all_users()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Broadcasting your messages...'
+    )
+    start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
     blocked = 0
     deleted = 0
     failed =0
+
     success = 0
-    start_time = time.time()
     async for user in users:
-        user_id = int(user['id'])
-        try:
-            await b_msg.copy(chat_id=user_id)
+        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        if pti:
             success += 1
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await b_msg.copy(chat_id=user_id)
-        except InputUserDeactivated:
-            await db.delete_user(user_id)
-            logging.info(f"{user_id} - Removed from Database, since deleted account.")
-            deleted += 1
-        except UserIsBlocked:
-            logging.info(f"{user_id} -Blocked the bot.")
-            blocked+=1
-        except PeerIdInvalid:
-            await db.delete_user(int(user_id))
-            logging.info(f"{user_id} - PeerIdInvalid")
-            failed += 1
-        except Exception as e:
-            failed += 1
+        elif pti == False:
+            if sh == "Blocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
+                failed += 1
         done += 1
-        if not done % 100:
+        await asyncio.sleep(2)
+        if not done % 20:
             await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.delete()
-    await message.reply_text(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}", quote=True)
-
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
 
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
 async def group_broadcast(bot, message):
